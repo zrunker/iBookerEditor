445b19521798c97b4936f0b462ce3f12587246b1
吾非言 <1823132505@qq.com>
2018-09-01 10:26:18 +0800

    init Commit

---
+++ app/src/main/java/cc/ibooker/ibookereditor/activity/WelcomeActivity.java
@@ -0,0 +1,42 @@
+package cc.ibooker.ibookereditor.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+import cc.ibooker.ibookereditor.R;
+import cc.ibooker.ibookereditor.base.BaseActivity;
+
+/**
+ * 欢迎页面
+ */
+public class WelcomeActivity extends BaseActivity {
+    private Handler handler;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 进入首页
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (handler != null) {
+            handler.removeCallbacksAndMessages(null);
+            handler = null;
+        }
+    }
+}
